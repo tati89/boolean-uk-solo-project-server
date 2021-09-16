6efc93cb84600e3667cdd180af77a8b41c695487
tati89 <malena.one@gmail.com>
2021-09-16 15:37:45 +0100

    create, validate token functions created

---
+++ src/utils/authGenrator.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+config();
+
+const JWT_SECRET = process.env.JWT as string;
+
+export function createToken(payload: jwt.JwtPayload) {
+  return jwt.sign(payload, JWT_SECRET);
+}
+
+export function validateToken(token: string) {
+  return jwt.verify(token, JWT_SECRET);
+}
